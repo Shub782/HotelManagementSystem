551c4a49e80f9a387d232cc71f9ccc0ef27436ed
Shubham Prakash Aranha <shubhamaranha@gmail.com>
2026-04-03 08:33:00 +0530

    Refactor database connection and add test query
    Updated database connection method and added test query.

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
-include "config/db.php";
-echo "Database Connected Successfully";
+require_once 'config/db.php';
+
+echo "PDO Connected Successfully!";
+
+// Test query
+$result = $pdo->query("SELECT * FROM admin LIMIT 1");
+$row = $result->fetch(PDO::FETCH_ASSOC);
+
+echo "<pre>";
+print_r($row);
+echo "</pre>";
 ?>
